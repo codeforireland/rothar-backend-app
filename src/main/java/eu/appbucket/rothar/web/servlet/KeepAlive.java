7140f2b9dd8d2c0dec9c5ccd9de29525c1e77509
Adam Bednarski <abednarski79@gmail.com>
2014-12-17 23:40:30 +0000

    mapiaty dataty

--- src/main/java/eu/appbucket/rothar/web/servlet/KeepAlive.java
+++ src/main/java/eu/appbucket/rothar/web/servlet/KeepAlive.java
@@ -2,24 +2,37 @@ package eu.appbucket.rothar.web.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class KeepAlive extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
+	private static final long serialVersionUID = 1L;
+	private String appVersion;
+	
 	public KeepAlive() {
 		super();
 	}
 
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		appVersion = config.getInitParameter("version");
+	}
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		out.println("<h1>Rothar is alive</h1>");
+		Date currentTime = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+		String outputTemplate = "<h1>Rothar is alive, time: %s, version: %s</h1>";
+		String output = String.format(outputTemplate, formatter.format(currentTime), appVersion);
+		out.println(output);
 	}
 
 	protected void doPost(HttpServletRequest request,
