4e4fb94619ea603c6a52a85642c4ecb74bf0fe0a
Adam Bednarski <adam.bednarski@globoforce.com>
2015-11-17 22:55:30 +0000

    Fix the issue with env

--- src/main/java/eu/appbucket/rothar/core/service/SystemServiceImpl.java
+++ src/main/java/eu/appbucket/rothar/core/service/SystemServiceImpl.java
@@ -1,11 +1,13 @@
 package eu.appbucket.rothar.core.service;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SystemServiceImpl implements SystemService {
 
 	public int getSystemUserId() {
-		return Integer.valueOf(System.getProperty("USER_ID"));
+		String userId = System.getenv("USER_ID");
+        return Integer.valueOf(userId);
 	}
 }
